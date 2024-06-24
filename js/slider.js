77a83c2e8f4f2ee2947521116ffd31710780116f
Korolebi <korolebicontact@gmail.com>
2024-06-25 03:22:23 +0900

    initial setup with test images and js

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        'images\cat.jpg',
+        'images\mouse.jpg',
+        'images\sejong.jpg'
+    ];
+    const intervalTime = 3000; // 3 seconds
+    let currentIndex = 0;
+
+    const sliderComponent = document.getElementById('slider_component');
+    const imgElement = document.createElement('img');
+    imgElement.src = images[currentIndex];
+    sliderComponent.appendChild(imgElement);
+
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        imgElement.src = images[currentIndex];
+    }, intervalTime);
+});
